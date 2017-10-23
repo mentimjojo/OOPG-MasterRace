ddf4d624bf6c3498cc2354701887399b0b652ba5
Tim Nijborg <info@ict-focused.com>
2017-10-23 09:18:27 +0200

    Deleted waterworld

--- src/main/java/nl/han/ica/MasterRace/Maps/Map.java
+++ src/main/java/nl/han/ica/MasterRace/Maps/Map.java
@@ -3,7 +3,6 @@ package nl.han.ica.MasterRace.Maps;
 import nl.han.ica.OOPDProcessingEngineHAN.Objects.Sprite;
 import nl.han.ica.OOPDProcessingEngineHAN.Tile.TileMap;
 import nl.han.ica.OOPDProcessingEngineHAN.Tile.TileType;
-import nl.han.ica.waterworld.tiles.BoardsTile;
 
 public class Map {
 
